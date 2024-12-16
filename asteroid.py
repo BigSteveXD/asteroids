f87ceba56ef42cd1f08c2f3599baa77dddb1ffd3
BigSteveXD <24504153+BigSteveXD@users.noreply.github.com>
2024-12-16 08:20:57 -0800

    fix update method

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,12 @@
-from CircleShape import CircleShape
+import pygame
+from circleshape import CircleShape
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
         line_width = 2
-        self.x += (self.velocity * dt)####
-        pygame.draw.circle(screen, "white", self.x, self.y, self.radius, line_width)
+        pygame.draw.circle(screen, "white", self.position, self.radius, line_width)
 
-    def update(self):
+    def update(self, dt):
+        self.position += (self.velocity * dt)
