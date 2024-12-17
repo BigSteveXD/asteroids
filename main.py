07f72ea2e412f7562b1d0f3a9d8fee5c399d35b6
BigSteveXD <24504153+BigSteveXD@users.noreply.github.com>
2024-12-17 08:08:41 -0800

    add collision check for asteroids and shots

--- main.py
+++ main.py
@@ -17,18 +17,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     run = True
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     Shot.containers = (shots, updatable, drawable)
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+    
     player = Player(x,y)
     asteroid_field = AsteroidField()####
+
 
     while(run):
         for event in pygame.event.get():
@@ -43,6 +47,10 @@ def main():
             if(player.check_collision(x)):#asteroid.check_collision(player)
                 print("Game over!")
                 sys.exit()
+            for y in shots:
+                if(x.check_collision(y)):
+                    x.kill()
+                    y.kill()
 
         for x in drawable:
             x.draw(screen)
